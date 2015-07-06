ee580eebc67f0b64cfb3da35cbb2b370e17d3808
Konstantin Lobov <koss40er@gmail.com>
2015-07-06 16:17:26 +0300

    angular added

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var app = angular.module('eventList', []);
+    var app = angular.module('eventList', []);
+    app.controller('EvntController',function(){
+        this.news = evnts;
+    });
+    var evnts = [
+        {titl: 'angular title!', cntnt: 'angular content'},
+        {titl: ' snd angular title!', cntnt: 'another angular content'}
+    ];
